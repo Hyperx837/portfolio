5902598d40f6900a0131def526d977116b11ef1a
Anupama Dissanayake <61502343+Hyperx837@users.noreply.github.com>
2022-07-16 14:28:53 +0530

    initial commit

---
+++ components/NavBar.tsx
@@ -0,0 +1,10 @@
+import { NextComponentType } from "next";
+
+const NavBar: NextComponentType = () => {
+  return (
+    <nav>
+      <li className=""></li>
+    </nav>
+  );
+};
+export default NavBar;
