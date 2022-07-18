35f242f47088483b50ea624f045c98fd7079b94a
Anupama Dissanayake <61502343+Hyperx837@users.noreply.github.com>
2022-07-18 21:00:51 +0530

    goal: roughly finished landing page

--- components/About.tsx
+++ components/About.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { useTypewriter } from "react-simple-typewriter";
+import TypeWriter from "./TypeWriter";
 
 const About = () => {
   const [color, setColor] = useState("");
@@ -19,12 +20,12 @@ const About = () => {
       className="flex lg:flex-row pl-20  justify-around flex-col-reverse  "
       id="about"
     >
-      <div className="flex justify-center lg:w-[45%] sm:w-full sm:mt-20 lg:m-0">
+      <div className="flex justify-center lg:w-[45%] sm:w-full sm:mt-20 lg:m-0 overflow-clip rounded-full">
         <Image
-          src="/images/ava2.png"
+          src="/images/avatar3.png"
           alt="pic of my avatar"
-          width={400}
-          height={550}
+          width={560}
+          height={630}
         />
       </div>
       <div className="pl-2 flex flex-col w-full lg:w-[55%] pt-16">
@@ -39,6 +40,7 @@ const About = () => {
           I am a <span className={`${color} ml-4`}> {text}</span>
           <span className={`${color} blink`}>_</span>
         </span>
+        <TypeWriter />
         <button className="btn mx-auto lg:mx-0 py-4 mt-20 w-64 ">
           Hire Me
         </button>
